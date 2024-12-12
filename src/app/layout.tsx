f0c137050e59bdabb09dbd0e66548b5cab2a26e5
Ngo The Cao Son <ngothecaoson@gmail.com>
2024-12-12 10:58:41 +0700

    them Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,23 @@ export default function RootLayout({
       <html lang="en">
         <Head>
                 {/* Thêm thẻ meta vào đây */}
-              <meta name="zalo-platform-site-verification" content="Q_2i1A3JV0rgxEGWw_bKUZ3yn6FSYXKyDpWt" />
-        </Head>
+                <meta name="zalo-platform-site-verification" content="Q_2i1A3JV0rgxEGWw_bKUZ3yn6FSYXKyDpWt" />
+                
+                {/* Thêm title cho trang */}
+                <title>HappyFood - Nơi chia sẻ công thức nấu ăn</title>
+
+                {/* Thêm meta description */}
+                <meta name="description" content="Chia sẻ các công thức nấu ăn ngon, dễ làm từ các đầu bếp chuyên nghiệp. Khám phá ngay!" />
+
+                {/* Thêm meta image */}
+                <meta property="og:image" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/logo/Logo.png" />
+
+
+                {/* Thêm meta title cho các mạng xã hội */}
+                <meta property="og:title" content="HappyFood - Nơi chia sẻ công thức nấu ăn" />
+                <meta property="og:description" content="Khám phá những công thức nấu ăn dễ làm và hấp dẫn." />
+                <meta property="og:url" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/" />
+            </Head>
        <UserProvider>
         <RecipeProvider>
         <body className=" flex justify-center flex-col items-center  ">
