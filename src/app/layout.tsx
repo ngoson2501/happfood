e2a8c36ae2e3aafd159ccc463a79d1982f92811d
Ngo The Cao Son <ngothecaoson@gmail.com>
2024-12-12 11:18:57 +0700

    fix the meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 import { UserProvider } from "@/context/User-provider";
 import { RecipeProvider } from "@/context/RecipeContext";
 import { useAuth } from "../../hooks/useAuth";
-import Head from "next/head"; // Import Head từ Next.js
+
 export default function RootLayout({
     children,
   }: {
@@ -15,25 +15,7 @@ export default function RootLayout({
 
     return (
       <html lang="en">
-        <Head>
-                {/* Thêm thẻ meta vào đây */}
-                <meta name="zalo-platform-site-verification" content="Q_2i1A3JV0rgxEGWw_bKUZ3yn6FSYXKyDpWt" />
-                
-                {/* Thêm title cho trang */}
-                <title>HappyFood - Nơi chia sẻ công thức nấu ăn</title>
-
-                {/* Thêm meta description */}
-                <meta name="description" content="Chia sẻ các công thức nấu ăn ngon, dễ làm từ các đầu bếp chuyên nghiệp. Khám phá ngay!" />
-
-                {/* Thêm meta image */}
-                <meta property="og:image" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/logo/Logo.png" />
-
-
-                {/* Thêm meta title cho các mạng xã hội */}
-                <meta property="og:title" content="HappyFood - Nơi chia sẻ công thức nấu ăn" />
-                <meta property="og:description" content="Khám phá những công thức nấu ăn dễ làm và hấp dẫn." />
-                <meta property="og:url" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/" />
-            </Head>
+        
        <UserProvider>
         <RecipeProvider>
         <body className=" flex justify-center flex-col items-center  ">
@@ -52,7 +34,7 @@ export default function RootLayout({
           </body>
         </RecipeProvider>
        
-          </UserProvider>
+      </UserProvider>
       </html>
     )
   }
