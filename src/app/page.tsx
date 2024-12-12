0d400e20f148dcdeff3989e0cbcb83acd4cf68a6
Ngo The Cao Son <ngothecaoson@gmail.com>
2024-12-12 13:13:39 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,11 @@ import RecipesList from "@/components/RecipesList/RecipesList"
 import LearnMore from "@/components/LearnMore/LearnMore"
 import RecommendedList from "@/components/RecommendedList/RecommendedList";
 import Footer from "@/components/Footer/Footer";
-import Head from "next/head"; // Import Head từ Next.js
 
 
 function HomePage(){
     return(
         <>
-            <Head>
-                {/* Thêm thẻ meta vào đây */}
-                <meta name="zalo-platform-site-verification" content="Q_2i1A3JV0rgxEGWw_bKUZ3yn6FSYXKyDpWt" />
-                
-                {/* Thêm title cho trang */}
-                <title>HappyFood - Nơi chia sẻ công thức nấu ăn</title>
-
-                {/* Thêm meta description */}
-                <meta name="description" content="Chia sẻ các công thức nấu ăn ngon, dễ làm từ các đầu bếp chuyên nghiệp. Khám phá ngay!" />
-
-                {/* Thêm meta image */}
-                <meta property="og:image" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/logo/Logo.png" />
-
-
-                {/* Thêm meta title cho các mạng xã hội */}
-                <meta property="og:title" content="HappyFood - Nơi chia sẻ công thức nấu ăn" />
-                <meta property="og:description" content="Khám phá những công thức nấu ăn dễ làm và hấp dẫn." />
-                <meta property="og:url" content="https://front-end-happfood-fls98canu-ngosons-projects.vercel.app/" />
-            </Head>
             <div className="bg-white w-full flex gap-[20px] flex-col lg:px-[100px] lg:pt-[50px] h-fit ">
                 
             <Slider></Slider>
@@ -40,7 +20,6 @@ function HomePage(){
             
             </div>
             <Footer></Footer>     
-            
         </>
     )
 }
